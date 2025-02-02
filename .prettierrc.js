3202e839f9e8b69b8e33a4935f8a40899c462454
Deepen Timalsina <deepentimalsina05@gmail.com>
2025-02-02 23:26:14 +0545

    update: prettier introduced and pnpm lock file created

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  bracketSpacing: true,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  arrowParens: "always",
+};
